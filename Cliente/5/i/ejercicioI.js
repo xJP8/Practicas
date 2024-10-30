6d690fbf7206c288006d3b8b532f39c1323cc71a
Tu Nombre <you@example.com>
2024-10-30 16:40:00 +0100

    5I

--- Cliente/5/i/ejercicioI.js
+++ Cliente/5/i/ejercicioI.js
@@ -1,6 +1,7 @@
 const text = document.getElementById("text");
 const resultado = document.getElementById("resultado");
 const colores = document.getElementById("colores");
+const bk_color = document.getElementById("bk_color");
 const tamano = document.getElementById("tamano");
 
 
@@ -18,6 +19,38 @@ function cambiarTamanoTexto() {
     resultado.style.fontSize = tamano.value + "px"; 
 }
 
-function alternarSubreayado() {
-    resultado.style.blod = tamano.value + "px"; 
+function alternarSubrayado() {
+    if (resultado.style.textDecoration === "underline") {
+        resultado.style.textDecoration = "none";
+    } else {
+        resultado.style.textDecoration = "underline";
+    }
+}
+
+function alternarCursiva() {
+    if (resultado.style.fontStyle === "italic") {
+        resultado.style.fontStyle = "normal";
+    } else {
+        resultado.style.fontStyle = "italic";
+    }
+}
+
+function alternarNegrita() {
+    if (resultado.style.fontWeight === "bold") {
+        resultado.style.fontWeight = "normal";
+    } else {
+        resultado.style.fontWeight = "bold";
+    }
+}
+
+function cambiarFondo() {    
+    resultado.style.backgroundColor = bk_color.value;
+}
+
+function cambiarBorde(border_color) {    
+    resultado.style.borderColor = border_color;
+}
+
+function cambiarTipografia(fuente) {
+    resultado.style.fontFamily = fuente;
 }
