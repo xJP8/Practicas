43981a3a776b0cac01401f6f64c9dff13e6b1474
Tu Nombre <you@example.com>
2024-11-07 19:31:02 +0100

    2

---
+++ Cliente/Ejercicio 2/5/ejercicio5.js
@@ -0,0 +1,6 @@
+const tipografia = document.getElementById("tipografia");
+const txt = document.getElementById("txt");
+
+function cambiarTipografia(){
+    txt.style.fontFamily = tipografia.value;
+}
