7e71c979c487844b515467484ba9f6ac00969364
jesus <jesus@example.com>
2024-11-27 20:03:15 +0100

    P5

---
+++ Servidor/P15/oficines/search.php
@@ -0,0 +1,93 @@
+<?php
+session_start();
+$nombre = "";
+$oficinaEncontrada = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nombre = $_POST['oficina'];
+    if ($nombre == null || $nombre == "") {
+        header("Location: /oficines/oficines.php");
+        exit();
+    } else {
+        // Credenciales BBDD //
+        $servername = "localhost";
+        $username = "cliente";
+        $password = "";
+        $dbname = "jesusdb";
+
+        // Conexión BBDD//
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn->set_charset("utf8mb4");
+        
+        if ($conn->connect_error) {
+            die("Conexión fallida: " . $conn->connect_error);
+        }
+
+        // Preparación de la consulta //
+        $sql = "SELECT mapa, calle, cod_postal, horario FROM oficinas WHERE nombre = ?";
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bind_param("s", $nombre); // Introducción de datos //
+
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        // Comprobación del resultado //
+        if ($result->num_rows > 0) {
+            $oficinaEncontrada = true;
+            $row = $result->fetch_assoc();
+        }
+
+        $stmt->close();
+        $conn->close();
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/style.css">
+    <title>Search</title>
+</head>
+<body>
+    <header>
+        <h1>Bancos JP</h1>
+        <nav>
+            <a href="/index.php">Inicio</a>
+            <a href="/services/services.php">Servicios</a>
+            <a href="/oficines/oficines.php">Oficinas</a>
+            <a href="/contacts/contacts.php">Contacto</a>
+            <?php
+                // Si existe sessión se muestra Acceso. Si no, Cuentas y Cerrar Sesion //
+                if(!isset($_SESSION["nombre"])){
+                    echo '<a href="/clients/view/clients.php">Acceso clientes</a>';
+                } else{
+                    echo '<a href="/accounts/view/account.php">Cuentas</a>';
+                    echo '<a href="/clients/view/client_logout.php">Cerrar sesión</a>';
+                }
+            ?>
+        </nav>
+    </header>
+    <main>
+        <div>
+            <?php if ($oficinaEncontrada): ?>
+                <h2><?php echo $nombre; ?></h2>
+                <h3><?php echo $row["calle"]; ?></h3>
+                <h3><?php echo $row["cod_postal"]; ?></h3>
+                <h3><?php echo $row["horario"]; ?></h3>
+                <?php echo $row["mapa"]; ?>
+            <?php else: ?>
+                <h2>Error</h2>
+                <p>No se ha encontrado la oficina deseada.</p>
+            <?php endif; ?>
+        </div>
+    </main>
+
+    <footer>
+        <p>&copy; 2024 Bancos JP. Todos los derechos reser'$nombre'vados.</p>
+    </footer>
+</body>
+</html>
