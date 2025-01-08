587cefbdc9e191d6b9b9defc113aa6716d222fd0
jesus <jesus@example.com>
2025-01-08 16:32:26 +0100

    Main Fin

--- Cliente/trimestre_2/eventos/1/js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('mousemove', function(event) {
-    var posX = event.clientX;
-    var posY = event.clientY;
-    this.getElementById('posX').value = posX;
-    this.getElementById('posY').value = posY;
-});
