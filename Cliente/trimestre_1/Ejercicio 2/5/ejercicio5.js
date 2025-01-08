9c5bec6de2483b2d89f103beee3e216538f5c634
jesus <jesus@example.com>
2025-01-08 16:31:25 +0100

    Servidor

--- Cliente/trimestre_1/Ejercicio 2/5/ejercicio5.js
+++
@@ -1,6 +0,0 @@
-const tipografia = document.getElementById("tipografia");
-const txt = document.getElementById("txt");
-
-function cambiarTipografia(){
-    txt.style.fontFamily = tipografia.value;
-}
