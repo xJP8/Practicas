df62feaa98d4131d907de676f27172c4638b3ae5
jesus <jesus@example.com>
2025-02-13 21:01:25 +0100

    ang

--- trimestre_2/Angular/proyecto009/src/app/app.component.ts
+++ trimestre_2/Angular/proyecto009/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,28 +9,18 @@ import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  conversion = '';
-  potenciaNumero = '';
-  cantidad = '';
+  resultado='';
 
-  formularioConversion = new FormGroup({
-    numerodecimal: new FormControl(''),
-    base: new FormControl('octal'),
-    potencia: new FormControl('2'),
-    largo: new FormControl(true)
+  formularioContacto = new FormGroup({
+    nombre: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    mail: new FormControl('', [Validators.required, Validators.email]),
+    mensaje: new FormControl('', [Validators.required, Validators.maxLength(500)])
   });
 
   submit() {
-    if (this.formularioConversion.value.numerodecimal) {
-      if (this.formularioConversion.value.base == "hexadecimal")
-        this.conversion = parseInt(this.formularioConversion.value.numerodecimal).toString(16);
-      if (this.formularioConversion.value.base == "octal")
-        this.conversion = parseInt(this.formularioConversion.value.numerodecimal).toString(8);
-      if (this.formularioConversion.value.numerodecimal && this.formularioConversion.value.potencia)
-        this.potenciaNumero = Math.pow(parseInt(this.formularioConversion.value.numerodecimal), parseInt(this.formularioConversion.value.potencia)).toString();
-      if (this.formularioConversion.value.largo)
-        this.cantidad = this.formularioConversion.value.numerodecimal.length.toString();
-    }
+    if (this.formularioContacto.valid)
+      this.resultado = "Todos los datos son válidos";
+    else
+      this.resultado = "Hay datos inválidos en el formulario";
   }
-
 }
