9c1e0015bbf6f4bf4b835472675f8c9628ddf57f
Clase <Jesus@mail.com>
2025-03-11 21:13:30 +0100

    17

---
+++ T17_Libreria/js/script.js
@@ -0,0 +1,39 @@
+function buscar() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("mySearch");
+    filter = input.value.toUpperCase();
+    ul = document.getElementsByClassName("productos")[0];
+    li = ul.getElementsByClassName("producto");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("h3")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function filtrar() {
+    var select, filter, ul, li, i;
+    select = document.getElementById("filtro");
+    filter = select.value;
+    ul = document.getElementsByClassName("productos")[0];
+    li = ul.getElementsByClassName("producto");
+    for (i = 0; i < li.length; i++) {
+        if (filter === "none" || li[i].classList.contains(filter)) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function mostrarCarrito() {
+    alert("Carrito de la compra");
+}
+
+function anadirCarrito() {
+    alert("Producto añadido al carrito");
+}
